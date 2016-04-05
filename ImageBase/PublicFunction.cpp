b8eba94c7fdfa8b8df82f4dbeef37d89f91fb9fd
damant <jcai@mail.com>
2016-04-05 16:31:41 +0800

    publicfunction

--- ImageBase/PublicFunction.cpp
+++ ImageBase/PublicFunction.cpp
@@ -13,17 +13,14 @@ CPublicFunction::~CPublicFunction(void)
 
 
 void CPublicFunction::SR_JOR(void){
-	CMainFrame* mainFrame=(CMainFrame*)(AfxGetApp()->GetMainWnd());
-	mainFrame->ChangeProcessBar(true);
 	if(!SRJORInitialize()){
 		return;
 	}
 	int upscale=3;
-	CString imgfile="E:\\license.jpg";
+	CString imgfile=".\\testdata\\license.jpg";
 	mwArray mw_path(imgfile);
 	mwArray mw_upscale(1,1,mxDOUBLE_CLASS);
 	mw_upscale.SetData(&upscale,1);
-	mwArray mw_outfile("E:\\t9.bmp");
+	mwArray mw_outfile(".\\testdata\\result\\t9.bmp");
 	SuperResolutionJOR(mw_path,mw_upscale,mw_outfile);
-	mainFrame->ChangeProcessBar(false);
 }
